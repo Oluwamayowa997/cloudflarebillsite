6744d7288b6ad5de368cff167240d225f1bf764f
オメイ君 <94592045+Oluwamayowa997@users.noreply.github.com>
2022-12-14 09:33:24 +0100

    Delete cloudflarebillsite directory

--- cloudflarebillsite/src/jss/water.js
+++
@@ -1,7 +0,0 @@
-function displaywater(){
-    var obtwater=document.getElementById('waterb').value;
-    var intwater=parseInt(obtwater,10);
-    var water_cost=intwater;
-    document.getElementById('water').innerHTML="That will cost you "+water_cost+" naira.";
-
-}
